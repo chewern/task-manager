e7bd7314b44a25920e2d11d0800de3e0efd2c562
chewern <61139502+chewern@users.noreply.github.com>
2021-06-05 15:36:02 +0800

    added Button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
-const Header = (props) => {
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = ({ title }) => {
+  //{ title } is a destructor
   return (
-    <header>
-      <h1>{props.title}</h1>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Hello" />
     </header>
   );
+};
+
+Header.defaultProps = {
+  // defaultProps sets the default value for Header.title
+  title: "Task Manager",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  //this will specify that title ought to be string, and should not be left empty
 };
 
 export default Header;
