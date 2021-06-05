3be2f33d80c78a3a463b0bbf28af6dae73559d08
chewern <61139502+chewern@users.noreply.github.com>
2021-06-05 18:02:57 +0800

    added tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Running",
+      day: "15 Feb 2021 3:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Swimming",
+      day: "22 Feb 2021 3:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Sleeping",
+      day: "24 Feb 2021 8:30pm",
+      reminder: true,
+    },
+  ]);
+
   return (
     <div className="container">
       <Header title="Task List" />
+      <Tasks tasks={tasks} />
     </div>
   );
 };
